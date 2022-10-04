365529932ac1c7e515578421d7df646a6c4043d2
Govindaraj B <govidev1012000@gmail.com>
2022-10-04 11:54:01 +0000

    try

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>Here is govindaraj </code>
+          <code className={styles.code}>Here is govindaraj hello </code>
         </p>
 
         <div className={styles.grid}>
